3f7a935338aafa2c5e5992f20a44dc22806060cc
jgrac <jgrac@LAPTOP-IRKLNN0V.mshome.net>
2025-06-13 09:08:29 +0530

    worked on addition using scanner class

---
+++ C2TC_CoreJavaPrograms/src/day1/AdditionScanner.java
@@ -0,0 +1,21 @@
+package day1;
+import java.util.Scanner;
+
+public class AdditionScanner {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		
+		Scanner sc;
+        sc=new Scanner(System.in);
+        
+        System.out.println("Enter a");
+        a=sc.nextInt();
+        System.out.println("Enter b");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Addition:"+c);
+	}
+
+}
+

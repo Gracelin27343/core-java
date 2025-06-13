0f8a856867becbae53e0046abb33f6365cf8db3a
jgrac <jgrac@LAPTOP-IRKLNN0V.mshome.net>
2025-06-13 10:27:21 +0530

    worked on Datatypes

---
+++ C2TC_CoreJavaPrograms/src/day2/Datatypes.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int value1 = 63 / 5; //integer division
+		float value2 = 3001 / 61;
+		double value3 = 100d / 7d;
+		float value4 = 15/7.5f; //float division
+		System.out.println("value 1=" + value1); 
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		System.out.println("value 4=" + value4);
+		
+		
+		int marker = 1024;
+		//assigning expression to variable 
+		double percentage = marker * 0.56f;
+		System.out.println("percentage :" + percentage);
+
+	}
+
+}

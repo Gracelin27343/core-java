7ca2a557cebc74ea23020a052c1a6871291bc42e
jgrac <jgrac@LAPTOP-IRKLNN0V.mshome.net>
2025-06-13 11:49:04 +0530

    class inside another class

---
+++ C2TC_CoreJavaPrograms/src/day1/StudDemo.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class StudDemo {
+    public static void main(String[] args) {
+		Student st = new Student();
+        st.id =101;
+        st.name ="Grace";
+        st.age =20;
+        st.show();
+        //System.out.println(st.id +":"+ st.name + ":"+st.age);
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/day1/Student.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Student {
+		int id;
+		String name;
+		int age;
+		
+		public void show() {
+			System.out.println("Student class method");
+			System.out.println(id +":"+ name + ":"+age);
+	}
+
+}

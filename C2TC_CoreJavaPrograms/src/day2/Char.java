9ddfceebae89bede93f02a23bfcd25b5ced4d387
jgrac <jgrac@LAPTOP-IRKLNN0V.mshome.net>
2025-06-13 10:17:24 +0530

    worked on character by assinging unicode ascii and numbers

---
+++ C2TC_CoreJavaPrograms/src/day2/Char.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// assigning single character literal
+		char ch = 'g';
+		System.out.println(ch);
+
+		// assigning number to char
+		char ch1 = 78;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char var1 = '\u00B4';
+		System.out.println(var1);
+
+		// Unicode representation
+		char var2 = '\u20CC';
+		System.out.println(var2);
+
+		// ASCI code representation
+		int a = 'G';
+		System.out.println(a);
+
+	}
+
+}

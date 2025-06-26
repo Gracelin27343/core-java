0b457966b8e3e27d2cd7edcd4dc1ea5e78c42191
jgrac <jgrac@Gracelin>
2025-06-26 20:14:52 +0530

    Sample program on string ,wrapper class , object class

---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/daynine/StringSubclass.java
@@ -0,0 +1,11 @@
+package com.tnsif.daynine;
+
+//can't inherit final classes
+public class StringSubclass extends String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
